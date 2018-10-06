07b1c7caa23c5ed04d8dd70e817e253b2a027c12
Leonid Novak <lnovak77@gmail.com>
2018-10-06 10:11:38 -0700

    Chapter 1

--- app/src/main/java/com/novak/app/excercises/codecrack/Chapter1.java
+++ app/src/main/java/com/novak/app/excercises/codecrack/Chapter1.java
@@ -24,5 +24,111 @@ public class Chapter1 {
 		for (char c: s2.toCharArray()) {int val= hm.compute(c, (k,v)->(v==null)?-1:v-1);if (val<0) return false;}
 		return true;
 	}
+	
+	/* Excercise 1.4*/
+	public  boolean isPaliPermutation(String s1)
+	{
+	    int[] dict = new int[26];
+	    for (char c:s1.toLowerCase().toCharArray())
+	    {
+	    	if (!Character.isWhitespace(c)) dict[c-'a']++;
+	    }
+	    
+	    boolean hasOdd=false;
+	    for (int v:dict)
+	    {
+	    	if (v%2==1)
+	    	{
+	    		if (hasOdd) return false;
+	    		hasOdd=true;
+	    	}
+	    }
+	    return true;
+	}
+	
+	/*Excercise 1.5*/
+	public boolean oneAway(String s1, String s2)
+	{
+		int lgdif=s1.length()-s2.length();
+		if (Math.abs(lgdif)>1) return false;
+		String lg=(lgdif>0)?s1:s2;
+		String sh=(lg==s2)?s1:s2;
+		int posShf=0;		
+		boolean misMatchFound = false;
+		for (int i=0; i<sh.length(); i++)
+		{
+			if (lg.charAt(i+posShf)!=sh.charAt(i))
+			{
+				if (misMatchFound) return false;
+				misMatchFound=true;
+				if (lgdif!=0) {posShf=1; i--;}
+			}
+		}
+		return true;
+	}
+	
+	/*Excercise 1.6*/
+	public String stringCompression (String input)
+	{
+		StringBuilder stb= new StringBuilder(input.length());
+		char lchar=0;
+		int ccnt =0;
+		for (char c:input.toCharArray())
+		{
+			if (c==lchar) ccnt++;
+			else
+			{
+				if (lchar!=0) 
+					stb.append(lchar).append(ccnt);
+				lchar=c;
+				ccnt=1;
+				if (stb.length()>input.length()) return input;
+			}
+		}
+		if (lchar!=0)
+			stb.append(lchar).append(ccnt);
+		if (stb.length()>input.length()) return input;
+		return stb.toString();
+		
+	}
+	
+	/* Excercise 1.7 */
+	public void rotateMatrix(int[][] matrix)
+	{
+		int lgth=matrix.length;
+		int loops=lgth/2;
+		for (int i=0;i<loops;i++)
+		{
+			for (int j=i;j<matrix.length-i-1;j++)
+			{
+			
+				int tmp= matrix[lgth-1-j][i];
+				 matrix[lgth-1-j][i]= matrix[lgth-1-i][lgth-1-j];
+				 matrix[lgth-1-i][lgth-1-j]= matrix[j][lgth-i-1];
+				 matrix[j][lgth-i-1] = matrix[i][j];
+				 matrix[i][j]=tmp;
+			}
+		}
+	}
+	
+	/* Excercise 1.8 */
+	public void zeroMatrix(int[][] matrix)
+	{
+		int lgth=matrix.length;
+		int wdth=matrix[0].length;
+		boolean isZCol[]=new boolean[lgth];
+		boolean isZRow[]=new boolean[wdth];
+		for (int i=0;i<lgth;i++)
+			for (int j=0;j<wdth;j++) if (matrix[i][j]==0) isZCol[j]=(isZRow[i]=true);
+		for (int i=0;i<lgth;i++)
+			for (int j=0;j<wdth;j++) if ( isZCol[j]||isZRow[i]) matrix[i][j]=0;
+			
+	}
+	/* Excercise 1.9 */
+	public boolean isRotation(String s1, String s2)
+	{
+		if (s1.length()!=s2.length()) return false;
+		return (s1+s1).contains(s2);
+	}
 
 }
--- app/src/test/java/com/novak/app/excercises/codecrack/Chapter1Tests.java
+++ app/src/test/java/com/novak/app/excercises/codecrack/Chapter1Tests.java
@@ -29,5 +29,54 @@ public class Chapter1Tests {
 		Assertions.assertFalse(chapter.isPermutation("dasf","sdad"),"same size different chars");
 		Assertions.assertTrue(chapter.isPermutation("dasf","sfda"),"same size same chars");
 	}
+	
+	@Test
+	public void testIsPaliPermutation()
+	{
+		Assertions.assertEquals(true, chapter.isPaliPermutation("asdff fgga sd"));
+		Assertions.assertEquals(false, chapter.isPaliPermutation("asdff fgga sds"));
+		Assertions.assertEquals(true, chapter.isPaliPermutation("asdff gga sd"));
+	}
+	
+	@Test
+	public void testoneAway()
+	{
+		Assertions.assertEquals(true, chapter.oneAway("pale","ple"));
+		Assertions.assertEquals(true, chapter.oneAway("pales","pale"));
+		Assertions.assertEquals(true, chapter.oneAway("pale","bale"));
+		Assertions.assertEquals(false, chapter.oneAway("pale","bake"));
+	}
+	
+	@Test
+	public void testStringCompression()
+	{
+		Assertions.assertEquals("a2b1c5a3", chapter.stringCompression("aabcccccaaa"));
+		Assertions.assertEquals("ababcbabd", chapter.stringCompression("ababcbabd"));
+	}
+	
+	@Test
+	public void testRotate()
+	{
+		int[][] input= {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+		chapter.rotateMatrix(input);
+		Assertions.assertArrayEquals(new int[][] {{13,9,5,1},{14,10,6,2},{15,11,7,3},{16,12,8,4}}, 
+				input);
+	}
+	
+	@Test
+	public void testZeroMatrix()
+	{
+		int[][] input=  {{0,1,1},{1,0,1},{1,1,1}};
+		chapter.zeroMatrix(input);
+		Assertions.assertArrayEquals(input,new int[][] {{0,0,0},{0,0,0},{0,0,1}});
+	}
+	
+	@Test
+	public void testIsRotation()
+	{
+		
+		Assertions.assertEquals(chapter.isRotation("waterbottle", "bottlewater"), true);
+	}
+
 
 }
